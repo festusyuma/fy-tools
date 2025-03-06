7d2e0f1c01a6f6cd5226608ad3f0471e463c67d9
Festus <festusyuma@gmail.com>
2025-03-06 02:15:22 +0100

    update example

--- packages/examples/src/client/index.ts
+++ packages/examples/src/client/index.ts
@@ -1,15 +1,23 @@
 import { rpcClient } from '@fy-tools/rpc-client';
 import type { NestApp } from '../server-nestjs';
 
-const client = rpcClient<NestApp>();
+const client = rpcClient<NestApp>({
+  /** Use axios instance options here */
+  baseUrl: 'https://example.com',
+});
+
 client('user')
   /** Type safe payload */
-  .$post({
-    body: { name: 'Festus' },
-  })
+  .$post(
+    { body: { name: 'Festus' } },
+    {
+      /** Use axios request options here */
+      headers: {},
+    },
+  )
   .then((res) => {
     /** Type safe response */
-    console.log('res :: ', res.success);
+    console.log('res :: ', res.data);
   });
 
 client('user')
