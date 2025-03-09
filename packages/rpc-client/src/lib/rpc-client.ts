1a492963738aab0548e04ae93a98b6d5523a8345
Festus <festusyuma@gmail.com>
2025-03-09 01:25:07 +0100

    fix type inference issue

--- packages/rpc-client/src/lib/rpc-client.ts
+++ packages/rpc-client/src/lib/rpc-client.ts
@@ -1,28 +1,34 @@
 import { encode } from 'node:querystring';
 
 import type { App, Controller, Route } from '@fy-tools/rpc-server';
-import Axios, { AxiosError, type AxiosResponse } from 'axios';
+import Axios, {
+  AxiosError,
+  type AxiosRequestConfig,
+  type AxiosResponse,
+} from 'axios';
 
 import { AppError } from './app-error';
 import { methods } from './constants.js';
-import type {
-  FetcherOptions,
-  IsRouteMethod,
-  IsRoutePath,
-  ParseSchema,
-  RpcClientOptions,
-  StripNever,
-} from './types';
+import type { RpcClientOptions } from './types';
+import type { TypeOf, ZodObject, ZodRawShape } from 'zod';
 
-type Payload<R extends Route> = StripNever<{
-  body: ParseSchema<R['_body']>;
-  params: ParseSchema<R['_params']>;
-  query: ParseSchema<R['_query']>;
-}>;
+type ParseSchema<T> = T extends ZodObject<ZodRawShape> ? TypeOf<T> : never;
 
-export function rpcClient<T extends App<Controller<any, any>[]>>(
-  options?: RpcClientOptions
-) {
+type IsRoutePath<T, TP extends string> = T extends Route<TP> ? T : never;
+type IsRouteMethod<T, TM extends string> = TM extends keyof typeof methods
+  ? T extends Route<infer P, TM>
+    ? T
+    : never
+  : never;
+
+type StripNever<T> = {
+  [K in keyof T as T[K] extends never ? never : K]: T[K];
+};
+
+export function rpcClient<
+  T extends App<Controller<any, any>[]>,
+  FetcherOptions = Omit<AxiosRequestConfig<T>, 'method' | 'data'>
+>(options?: RpcClientOptions) {
   type Controllers = T['_controllers'][number];
   type Routes = Controllers['_routes'][number];
 
@@ -30,43 +36,54 @@ export function rpcClient<T extends App<Controller<any, any>[]>>(
     baseURL: options?.baseUrl,
   });
 
-  return <TPath extends Routes['_path']>(url: TPath) => {
-    type PRoute = IsRoutePath<Routes, TPath>;
+  async function req(
+    url: string,
+    method: string,
+    payload?: any,
+    options?: FetcherOptions
+  ) {
+    const parsedUrl = (url as string).replace(/:\w+/g, (match) => {
+      const param = payload?.params?.[match.slice(1)];
+      return param ?? match;
+    });
 
-    async function req<Response>(
-      method: string,
-      payload?: any,
-      options?: FetcherOptions
-    ) {
-      const parsedUrl = (url as string).replace(/:\w+/g, (match) => {
-        const param = payload?.params?.[match.slice(1)];
-        return param ?? match;
+    try {
+      return await axios.request({
+        url: parsedUrl + `?${encode(payload?.query ?? {})}`,
+        method,
+        data: payload?.body,
+        ...(options ?? {}),
       });
-
-      try {
-        return await axios.request<Response>({
-          url: parsedUrl + `?${encode(payload?.query ?? {})}`,
-          method,
-          data: payload?.body,
-          ...(options ?? {}),
-        });
-      } catch (e) {
-        throw new AppError(e as AxiosError);
-      }
+    } catch (e) {
+      throw new AppError(e as AxiosError);
     }
+  }
 
+  type Payload<R extends Route> = StripNever<{
+    body: ParseSchema<R['_body']>;
+    params: ParseSchema<R['_params']>;
+    query: ParseSchema<R['_query']>;
+  }>;
+
+  type PRoute<TPath extends string> = IsRoutePath<Routes, TPath>;
+  type MRoute<TPath extends string, M extends string> = IsRouteMethod<
+    PRoute<TPath>,
+    M
+  >;
+
+  return <TPath extends Routes['_path'] & string>(url: TPath) => {
     return Object.fromEntries(
       Object.entries(methods).map((p) => [
         p[0],
         (payload?: unknown, options?: FetcherOptions) =>
-          req(p[1], payload, options),
+          req(url, p[1], payload, options),
       ])
     ) as {
-      [key in PRoute['method']]: (
-        payload: Payload<IsRouteMethod<PRoute, key>>,
+      [key in PRoute<TPath>['method']]: (
+        payload: Payload<MRoute<TPath, key>>,
         options?: FetcherOptions
       ) => Promise<
-        AxiosResponse<ParseSchema<IsRouteMethod<PRoute, key>['_response']>>
+        AxiosResponse<ParseSchema<MRoute<TPath, key>['_response']>>
       >;
     };
   };
--- packages/rpc-client/src/lib/types.ts
+++ packages/rpc-client/src/lib/types.ts
@@ -1,31 +1,10 @@
 import type { App } from '@fy-tools/rpc-server';
-import type { Route } from '@fy-tools/rpc-server';
-import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import type { AxiosResponse } from 'axios';
 import type { TypeOf, ZodObject, ZodRawShape } from 'zod';
-
-import type { HttpMethod } from './constants';
 
 export type RpcClientOptions = {
   baseUrl?: string;
 };
-
-export type ParseSchema<T> = T extends ZodObject<ZodRawShape>
-  ? TypeOf<T>
-  : never;
-
-export type StripNever<T> = {
-  [K in keyof T as T[K] extends never ? never : K]: T[K];
-};
-
-export type IsRoutePath<T, TP extends string> = T extends Route<TP> ? T : never;
-export type IsRouteMethod<T, TM extends HttpMethod> = T extends Route<
-  infer P,
-  TM
->
-  ? T
-  : never;
-
-export type FetcherOptions = Omit<AxiosRequestConfig, 'method' | 'data'>;
 
 export type ApiRouteFunction<
   Arguments = any,
